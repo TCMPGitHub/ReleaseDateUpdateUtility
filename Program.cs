9c66c40d4641e57a3406d574f9a9f2cfd555fcdb
TCMPGitHub <103594034+TCMPGitHub@users.noreply.github.com>
2022-05-03 10:14:16 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace ReleaseDateUpdateUtility
+{
+    class Program
+    {
+        static bool mailSent = false;
+        static string mailbody = string.Empty;
+        static string DBBassConn = string.Empty;
+        static string FilePath = string.Empty;
+        static void Main(string[] args)
+        {
+            ReleaseDateUpdate lib = new ReleaseDateUpdate();
+            lib.DBBassConn = ConfigurationManager.ConnectionStrings["BassWeb"].ConnectionString;
+            string path = ConfigurationManager.AppSettings["FilePath"];
+            string filename = Path.Combine(path, "ReleaseDateChanges.csv");
+            if (File.Exists(filename))
+            {
+                lib.FilePath = filename;
+                mailbody = DateTime.Now.ToString() + " Start Releasedate update process..." + Environment.NewLine;
+                string retmessge = lib.Import();
+                mailbody = mailbody + DateTime.Now.ToString()  + " " + retmessge + Environment.NewLine;
+                //rename file with timestamp
+                var newFile = Path.Combine(path, "ReleaseDateChanges" + DateTime.Now.ToString("yyyyMMddhhss") + ".csv");
+                System.IO.File.Move(filename, newFile);
+                string rename = " ReleaseDateChanges.csv rename to ReleaseDateChanges" + DateTime.Now.ToString("yyyyMMddhhss") + ".csv";
+                LogWriter.LogMessageToFile(rename);
+                mailbody = mailbody + DateTime.Now.ToString() + rename + Environment.NewLine;
+            }
+            else
+            {
+                mailbody = mailbody + DateTime.Now.ToString() + " " + filename + " File Not Found..." + Environment.NewLine;
+            }
+
+            LogWriter.LogMessageToFile("Send email to TCMP ..." + DateTime.Now.ToString());
+            mailbody = mailbody + DateTime.Now.ToString() + " Send email to TCMP ...";
+            SendMail();
+            return;
+        }
+
+        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
+
+            if (e.Cancelled)
+            {
+                Console.WriteLine("[{0}] Send canceled.", token);
+            }
+            if (e.Error != null)
+            {
+                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+            }
+            else
+            {
+                LogWriter.LogMessageToFile(" Message sent.");
+            }
+            mailSent = true;
+        }
+
+        private static void SendMail()
+        {
+            SmtpClient client = new SmtpClient("smtp.cdcr.ca.gov");
+            MailAddress from = new MailAddress("FDCDBA3_TCMP@cdcr.ca.gov", "TCMP", System.Text.Encoding.UTF8);
+            // Set destinations for the e-mail message.
+            MailAddressCollection toAddresses = new MailAddressCollection();
+            toAddresses.Add(new MailAddress("Quentin.Miller@cdcr.ca.gov"));
+            //toAddresses.Add(new MailAddress("carol.xu@cdcr.ca.gov"));
+
+            // Specify the message content.
+            MailMessage message = new MailMessage();
+            message.From = from;
+            message.To.Add(toAddresses.ToString());
+            message.CC.Add("carol.xu@cdcr.ca.gov");
+            message.Body = "Release Data updated Completed at " + DateTime.Now + ":";
+
+
+            // Include some non-ASCII characters in body and subject.
+            message.Body = mailbody + Environment.NewLine;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.Subject = "Release Date Changed!";
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+            // Set the method that is called back when the send operation ends.
+            client.SendCompleted += new 
+            SendCompletedEventHandler(SendCompletedCallback);
+            // The userState can be any object that allows your callback 
+            // method to identify this send operation.
+            string userState = "Update Data";
+            client.SendAsync(message, userState);
+            string answer = Console.ReadLine();
+            // Clean up.
+            message.Dispose();
+            System.Environment.Exit(0);
+            //return;
+        }
+    }
+}
---
+++ ReleaseDateUpdate.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ReleaseDateUpdateUtility
+{
+    public class ReleaseDateUpdate 
+    {
+        public string DBBassConn { get; set; }
+        public string DBPatsConn { get; set; }
+
+        public string FilePath { get; set; }
+        public string ErrorMessage { get; set; }
+
+        //public event EventHandler Disposed;
+        public string  Import()
+        {
+            LogWriter.LogMessageToFile("Get all CDCRs from import file.");
+            //string excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\\DHCSDates\\Q42017Results.xlsx;Extended Properties=\"Excel 12.0;HDR=YES;\"";
+            //string CSVFileConnectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"text;HDR=Yes;FMT=Delimited\";", Path.GetDirectoryName(FilePath));
+            string CSVFileConnectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"text;HDR=Yes;FMT=Delimited\";", Path.GetDirectoryName(FilePath));
+            try
+            {
+                DataTable dt = new DataTable();
+                using (OleDbConnection con = new OleDbConnection(CSVFileConnectionString))
+                {
+                    con.Open();
+                    var csvQuery = string.Format("select [CDCNUMBER],[SCHEDULEDRELEASEDATE] from [{0}]", Path.GetFileName(FilePath));
+                    //using (OleDbDataAdapter da = new OleDbDataAdapter(csvQuery, con))
+                        
+                //using (OleDbConnection con = new OleDbConnection(CSVFileConnectionString))
+                //{
+                //    con.Open();
+                    //var csvQuery = string.Format("select [CDCNUMBER],[SCHEDULEDRELEASEDATE] from[{0}]", Path.GetFileName(FilePath));
+                    using (OleDbDataAdapter da = new OleDbDataAdapter(csvQuery, con))
+                    {
+                        try
+                        {
+                            da.Fill(dt);
+                        }
+                        catch (Exception ex)
+                        {
+                            LogWriter.LogMessageToFile(ex.Message);
+                        }
+                    }
+
+                    dt.TableName = "ReleaseDateChanges";
+                }
+
+                LogWriter.LogMessageToFile("Read data from file Complated");
+
+                if (UpdateReleaseDates(dt))
+                {
+                    LogWriter.LogMessageToFile("Total " + dt.Rows.Count + " CDCRs Updated.");
+                    return "Total " + dt.Rows.Count + " CDCRs Updated.";
+                }
+                else
+                {
+                    LogWriter.LogMessageToFile("Failed to update CDCRs.");
+                    return "Failed to update CDCRs.";
+                }
+            }
+            catch (Exception ex)
+            {
+                LogWriter.LogMessageToFile(ex.Message);
+                return ex.Message;
+            }
+        }
+
+        private bool UpdateReleaseDates(DataTable dt)
+        {
+            try
+            {
+                //Update DHCSDates only for Medical
+                using (SqlConnection cnz = new SqlConnection(DBBassConn))
+                {
+                    try
+                    {
+                        cnz.Open();
+                        SqlCommand cmdzs = new SqlCommand();
+                        SqlParameter parameter = new SqlParameter();
+                        cmdzs.Connection = cnz;
+                        cmdzs.Parameters.AddWithValue("@ReleaseDateChanges", dt);
+                        cmdzs.CommandText = "spUpdateReleaseDates";
+                        cmdzs.CommandType = CommandType.StoredProcedure;
+                        cmdzs.CommandTimeout = 300;
+                        cmdzs.ExecuteNonQuery();
+                        return true;
+                    }
+                    catch (SqlException err)
+                    {
+                        LogWriter.LogMessageToFile(err.Message);
+                    }
+                    finally
+                    {
+                        cnz.Close();
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogWriter.LogMessageToFile(ex.Message);
+                return false;
+            }
+
+        }
+
+        //public int ImportDate()
+        //{
+
+        //    DataTable dt = new DataTable("CSVTable");
+        //    LogWriter.LogMessageToFile("Read dates from file: ");
+
+        //    string CSVFileConnectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"text;HDR=Yes;FMT=Delimited\";", Path.GetDirectoryName(FilePath));
+        //    using (OleDbConnection con = new OleDbConnection(CSVFileConnectionString))
+        //    {
+        //        con.Open();
+        //        var csvQuery = string.Format("select * from [{0}]", Path.GetFileName(FilePath));
+        //        using (OleDbDataAdapter da = new OleDbDataAdapter(csvQuery, con))
+        //        {
+        //            da.Fill(dt);
+
+        //        }
+        //        if (dt.Rows.Count > 0)
+        //        {
+        //            using (SqlBulkCopy bulkcopy = new SqlBulkCopy(DBBassConn))
+        //            {
+        //                bulkcopy.ColumnMappings.Add("CDCNUMBER", "CDCNUMBER");
+        //                bulkcopy.ColumnMappings.Add("SCHEDULEDRELEASEDATE", "SCHEDULEDRELEASEDATE");
+
+        //                bulkcopy.DestinationTableName = "ReleaseDateChanges";
+        //                bulkcopy.BatchSize = 0;
+        //                bulkcopy.WriteToServer(dt);
+        //                bulkcopy.Close();
+        //            }
+        //        }
+        //    }
+        //    if (dt.Rows.Count > 0)
+        //    {
+        //        var query = @"UPDATE e SET e.[ReleaseDate]= r.[SCHEDULEDRELEASEDATE] FROM [BassWeb].[dbo].[Episode] AS e 
+        //                               INNER JOIN [BassWebTest].[dbo].[ReleaseDateChanges] AS r ON e.[CDCRNum] = r.[CDCNUMBER]
+        //                      UPDATE e SET e.[ReleaseDate]= r.[SCHEDULEDRELEASEDATE] FROM [PatsWebV2].[dbo].[Episode] AS e 
+        //                               INNER JOIN [BassWebTest].[dbo].[ReleaseDateChanges] AS r ON e.[CDCRNum] = r.[CDCNUMBER]
+        //                      TRUNCATE TABLE [BassWebTest].[dbo].[ReleaseDateChanges]";
+        //        try
+        //        {
+        //            using (SqlConnection conn = new SqlConnection(DBBassConn))
+        //            {
+        //                conn.Open();
+        //                SqlCommand command = new SqlCommand(query, conn);
+        //                command.CommandTimeout = 3000;
+        //                command.ExecuteNonQuery();
+        //            }
+        //            return dt.Rows.Count;
+        //        }
+        //        catch (Exception ex) { } // Handle exception properly           
+        //    }
+        //    return 0;
+        //}
+
+        //protected virtual void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        // Free other state (managed objects).
+        //    }
+        //}
+
+        //public void Dispose()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        ~ReleaseDateUpdate() { }
+        //{
+        //    Dispose(false);
+        //}
+    }
+}
